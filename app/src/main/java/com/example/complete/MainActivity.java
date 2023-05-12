13cdd00b88818b45312d1bf00349f4bc389ef3a7
DevJaiswal79 <devj8496@gmail.com>
2023-05-12 23:44:50 +0530

    Added [Main layout , Menu , Manifest.xml , strings.xml]

---
+++ app/src/main/java/com/example/complete/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.complete;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends Activity implements OnItemClickListener
+{
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	Intent intent;
+
+	ListView lv;
+	String[] items ={"Check Version Info","Proximity Sensor","Touch Sensor","Light Sensor","Battery Indicator"};
+	@SuppressLint("SuspiciousIndentation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		lv= findViewById(R.id.listView1);
+		ArrayAdapter<String> adapt= new ArrayAdapter<>(this, R.layout.extra, items);
+		lv.setAdapter(adapt);
+		lv.setOnItemClickListener(this);
+	}
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int pos,long id) {
+		// TODO Auto-generated method stub
+
+
+	}
+
+}
+		
---
+++ app/src/main/java/com/example/complete/Splash.java
@@ -0,0 +1,38 @@
+package com.example.complete;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		Thread timer=new Thread(){
+			public void run(){
+				try{
+					sleep(5000);
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}finally
+				{
+					Intent j=new Intent(getApplicationContext(),MainActivity.class);
+					startActivity(j);
+				}
+			}
+		};
+		timer.start();
+	    
+	}
+	
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+
+}
